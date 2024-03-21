942e93197174b9f69171af7ff8ea50f0aad82d6b
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-03-20 21:08:18 -0300

    ajustes

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     
     path('cadastrar_vendedor/', views.cadastrar_vendedor, name="cadastrar_vendedor"),
     path('login/', views.login, name="login"),
-    path('sair/', views.logout, name="sair")
+    path('sair/', views.logout, name="sair"),
+    path('excluir_usuario/<str:id>/', views.excluir_usuario, name="excluir_usuario")
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -5,6 +5,7 @@ from .models import Users
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib import auth
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -12,7 +13,8 @@ from django.contrib import auth
 @has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
     if request.method == "GET":
-        return render(request, 'cadastrar_vendedor.html')
+        vendedores = Users.objects.filter(cargo="V")
+        return render(request, 'cadastrar_vendedor.html', {'vendedores': vendedores})
     if request.method == "POST":
         email = request.POST.get('email')
         senha = request.POST.get('senha')
@@ -49,3 +51,10 @@ def login(request):
 def logout(request):
     request.session.flush()
     return redirect(reverse('login'))
+
+
+@has_permission_decorator('cadastrar_vendedor')
+def excluir_usuario(request, id):
+    vendedor = get_object_or_404(Users, id=id)
+    vendedor.delete()
+    return redirect(reverse('cadastrar_vendedor'))
